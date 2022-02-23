1949ecdf5756ebb71256085cab4d7853b97499f2
Daniel <denenewton@icloud.com>
2022-02-23 15:19:48 -0300

    criation of home about e contact page

--- src/js/index.js
+++ src/js/index.js
@@ -24,3 +24,50 @@ function colorLink(){
 }
 
 linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+     reset: true
+});
+
+sr.reveal('.home__data ',{}); 
+sr.reveal('.home__photo',{delay: 700}); 
+sr.reveal('.home__title, home__subtitle',{ delay: 300}); 
+sr.reveal(' .home__name',{ delay: 400}); 
+sr.reveal(' .home__button',{delay: 500}); 
+
+
+sr.reveal('.about__photo',{delay: 700}); 
+sr.reveal('.about__title, about__subtitle',{ delay: 300}); 
+sr.reveal('.about__info',{ delay: 400}); 
+sr.reveal(' .home__button',{delay: 500}); 
+
+sr.reveal('.contact__title',{delay: 700}); 
+sr.reveal('.contact__input, about__subtitle',{ delay: 400}); 
+sr.reveal('.contact__textarea',{ delay: 500}); 
+sr.reveal(' .contact__button',{delay: 700}); 
+
